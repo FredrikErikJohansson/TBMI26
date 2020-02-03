5b6472805d1a82c9bbefe34b9469e5e0e0a730f5
FredrikErikJohansson <fredrik96.johansson@gmail.com>
2020-02-03 09:46:48 +0100

    Updated KNN

--- A1_Supervised/kNN/kNN.m
+++ A1_Supervised/kNN/kNN.m
@@ -13,30 +13,26 @@ classes = unique(LTrain);
 NClasses = length(classes);
 
 % Add your own code here
-LPred = zeros(size(X,1),1);
-distances = repmat(1000,size(X,1),1);
+LPred = zeros(length(X),1);
+distances = repmat(1000,length(X),1);
 matches = zeros(k,1);
 
 for i = 1:length(X)
     for j = 1:length(XTrain)  
-        distances(j,1) = norm(X(i,:)-XTrain(j,:));
+        distances(j) = norm(X(i,:)-XTrain(j,:));
     end
     
     %Store k closest points
     [~,closestPoints] = mink(distances(:,1),k);
    
-    %Find the prediced classes
-    for z = 1:NClasses
-       for w = 1:k
-           if(LTrain(closestPoints(w,1),1) == classes(z,1))
-               matches(w) = classes(z,1);
-           end
-       end
+    %Store the prediced classes
+    for z = 1:k
+        matches(z) = LTrain(closestPoints(z),1);
     end
     
     %Find the the class with most occurrences
     bestMatch = mode(matches);    
-    LPred(i,1) = bestMatch;
+    LPred(i) = bestMatch;
 end
 
 end
